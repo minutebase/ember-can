f1d6681ae1fa74045b29e31723035aa75e620beb
Richard Livsey <rlivsey@MacBook-Pro.local>
2014-11-08 22:06:09 +0000

    update resolver pluralization rules so we can use 'ability' as the container type and not 'activitie'

--- addon/utils/can.js
+++ addon/utils/can.js
@@ -3,12 +3,12 @@ import { normalize } from '../ability';
 
 export default function(container, abilityName, resource) {
   var name         = normalize(abilityName);
-  var resolver     = container.lookup("abilitie:resolver");
+  var resolver     = container.lookup("ability:resolver");
   var abilityClass = resolver.lookup(name, resource);
 
   Ember.assert("No ability handler class setup for "+abilityName, abilityClass);
 
-  var ability = container.lookup("abilitie:"+abilityClass);
+  var ability = container.lookup("ability:"+abilityClass);
 
   Ember.assert("Expected to find ability class for "+abilityClass+" but none was found, make sure you've added one", ability);
 
--- app/initializers/setup-ember-can-injections.js
+++ app/initializers/setup-ember-can-injections.js
@@ -3,13 +3,18 @@ import config from '../config/environment';
 export default {
   name: 'setup-ember-can-injections',
   initialize: function(container, application) {
+
+    // add pluralization rule for activity -> activities to the resolver
+    // TODO - update this when there's a better way of accessing the resolver
+    container.resolver.__resolver__.pluralizedTypes['ability'] = 'abilities';
+
     var canConfig = config['ember-can'];
     var injections = canConfig.inject || {};
 
     var name, injection;
     for (name in injections) {
       injection = injections[name];
-      application.inject('abilitie', name, injection);
+      application.inject('ability', name, injection);
     }
   }
 };
