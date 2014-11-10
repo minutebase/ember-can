de78a50a86873d07d08e4db8d81bc9c1b7d457c6
Richard Livsey <richard@livsey.org>
2014-11-10 21:48:04 +0000

    abilities are not singletons

--- app/initializers/setup-ember-can-injections.js
+++ app/initializers/setup-ember-can-injections.js
@@ -4,6 +4,9 @@ export default {
   name: 'setup-ember-can-injections',
   initialize: function(container, application) {
 
+    // make sure we create new ability instances each time, otherwise we stomp on each other's models
+    container.optionsForType('ability', { singleton: false });
+
     // add pluralization rule for activity -> activities to the resolver
     // TODO - update this when there's a better way of accessing the resolver
     container.resolver.__resolver__.pluralizedTypes['ability'] = 'abilities';
