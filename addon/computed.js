1301f144770409850452e966b78c55d8fc5dfaa9
Miguel Camba <miguel.camba@gmail.com>
2015-04-01 10:29:41 +0100

    Don't singularize the type
    Depending on your structure this might not be the right thing to do.
    If you name your ability `posts.js` or using the pod structyre it
    lives in `posts/ability.js` the helper will be `{{if (can "write posts")}}`
    
    If you name your file in a singular way it will be `{{if (can "write post"}}`
    as it used to be.

--- addon/computed.js
+++ addon/computed.js
@@ -1,19 +1,16 @@
 import Ember from 'ember';
 
-import { normalizeType } from 'ember-can/utils/normalize';
-
 var get = Ember.get;
 var set = Ember.set;
 
 export default {
-  ability: function(typeName, resourceName) {
+  ability: function(type, resourceName) {
     if (arguments.length === 1) {
-      resourceName = typeName;
+      resourceName = type;
     }
 
     return Ember.computed(resourceName, function() {
       var container = this.container;
-      var type      = normalizeType(typeName);
       var ability   = container.lookup("ability:"+type);
 
       Ember.assert("No ability class found for "+type, ability);
--- addon/utils/normalize.js
+++ addon/utils/normalize.js
@@ -14,8 +14,7 @@ var stopwords = [
 export function normalizeCombined(abilityName) {
   var parts   = abilityName.split(' ');
 
-  var typeStr = parts.pop();
-  var type    = normalizeType(typeStr);
+  var type = parts.pop();
 
   var last = parts[parts.length-1];
   if (stopwords.indexOf(last) !== -1) {
@@ -32,8 +31,4 @@ export function normalizeCombined(abilityName) {
 
 export function normalizeAbility(ability) {
   return 'can'+classify(ability);
-}
-
-export function normalizeType(type) {
-  return singularize(type);
 }
