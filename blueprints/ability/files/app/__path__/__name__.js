8cbf22cb4d58d1f252ba7af267dfcaf36beb122d
Ilya Radchenko <ilya@burstcreations.com>
2015-04-27 11:31:30 -0400

    Add ability blueprints
    Usage `ember g ability [name] [--pod]`

---
+++ blueprints/ability/files/app/__path__/__name__.js
@@ -0,0 +1,4 @@
+import { Ability } from 'ember-can';
+
+export default Ability.extend({
+});
---
+++ blueprints/ability/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = {
+  description: 'Generates an ember-can ability',
+
+  anonymousOptions: [
+    'name'
+  ],
+
+  fileMapTokens: function() {
+    return {
+      __name__: function(options) {
+        // The name defaults to the blueprint name, which is 'fragment', but
+        // it needs to be named 'model' for the resolver to find it
+        if (options.pod) {
+          return 'ability';
+        }
+        return options.dasherizedModuleName;
+      },
+      __path__: function(options) {
+        if (options.pod) {
+          return path.join(options.podPath, options.dasherizedModuleName);
+        }
+        // In non-pod format, fragments go in the 'models' directory
+        return 'abilities';
+      }
+    };
+  },
+
+  locals: function(options) {
+    return this.lookupBlueprint('model').locals(options);
+  }
+};
