704c19157b426da7f05f58ef5b1771bdae1bc1b5
Richard Livsey <richard@livsey.org>
2015-05-11 10:54:36 +0100

    Update to ember-cli 0.2.3

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -8,7 +8,6 @@ module.exports = function(environment) {
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
-        'ember-htmlbars': true
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       }
@@ -20,18 +19,18 @@ module.exports = function(environment) {
     }
   };
 
+  if (environment === 'development') {
   ENV['ember-can'] = {
     inject: {
       session: "session:main"
     }
   }
 
-  if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
