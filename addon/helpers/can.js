5b905192d6f9160da320b40417d909ff320382ac
Richard Livsey <richard@livsey.org>
2015-06-13 23:24:26 +0100

    Use Ember.Helper

--- addon/helpers/can.js
+++ addon/helpers/can.js
@@ -1,73 +1,30 @@
 import Ember from 'ember';
 import { normalizeCombined } from '../utils/normalize';
 
-// TODO - find a better way of hooking up the ability to the view scope
-// currently we set a unique property on the controller, which is rather nasty
+export default Ember.Helper.extend({
+  compute([name, resource], hash) {
+    const names   = normalizeCombined(name);
+    const ability = this.container.lookup("ability:"+names.type);
 
-var get = Ember.get;
-var set = Ember.set;
+    Ember.assert("No ability type found for "+names.type, ability);
 
-var _guid = 0;
-function guid() {
-  return "_ability-"+(++_guid);
-}
+    ability.set("model", resource);
+    ability.setProperties(hash);
 
-function bindViewProperty(ability, view, from, to) {
-  var stream;
-  if (from.isStream) {
-    stream = from;
-  } else {
-    stream = view.getStream(from);
-  }
-
-  set(ability, to, stream.value());
-
-  stream.subscribe(function(str) {
-    set(ability, to, str.value());
-  });
-}
-
-export default function(properties, hash, options, env) {
-  var abilityName  = properties[0];
-  var resource     = properties[1];
-  var view         = env.data.view;
-  var container    = view.container;
-  var controller   = get(view, "controller");
-
-  var names   = normalizeCombined(abilityName);
-  var ability = container.lookup("ability:"+names.type);
-
-  Ember.assert("No ability type found for "+names.type, ability);
-
-  if (resource) {
-    bindViewProperty(ability, view, resource, "model");
-  }
-
-  // bind additional properties
-  var key, prop;
-  for (key in hash) {
-    prop = hash[key];
-    if (prop.isStream || options.hashTypes && options.hashTypes[key] === "ID") {
-      bindViewProperty(ability, view, prop, key);
-    } else {
-      set(ability, key, prop);
+    if (this._ability) {
+      this._ability.removeObserver(this._abilityProp, this, "recompute");
     }
+
+    this._ability     = ability;
+    this._abilityProp = names.ability;
+
+    ability.addObserver(names.ability, this, "recompute");
+
+    return ability.get(names.ability);
+  },
+
+  destroy() {
+    this._ability.removeObserver(this._abilityProp, this, "recompute");
+    return this._super();
   }
-
-  // sets the ability to a unique property on the controller so that we can bind to it
-  // TODO - find a better way of doing this, ideally we shouldn't need to touch the controller at all
-  var holder = get(controller, "_abilities");
-  if (!holder) {
-    holder = Ember.Object.create();
-    set(controller, "_abilities", holder);
-  }
-
-  var id = guid();
-  set(holder, id, ability);
-
-  // binds to abilities._ability-xxx.canYYY
-  // where xxx is the ability instance and YYY is the property on that
-  var propName = ["controller._abilities", id, names.ability].join(".");
-
-  return view.getStream(propName);
-}
+});
