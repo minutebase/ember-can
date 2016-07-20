37bb3c0e46ba38f883bdc416d849e9b3d549ef48
Christopher Troup <minichate@gmail.com>
2016-07-20 15:59:19 -0400

    Typo in spelling of `additionalProperties` (#43)

--- addon/can-mixin.js
+++ addon/can-mixin.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   canService: Ember.inject.service('can'),
 
-  can: function(abilityName, resource, aditionalProperties) {
-    return this.get('canService').can(abilityName, resource, aditionalProperties);
+  can: function(abilityName, resource, additionalProperties) {
+    return this.get('canService').can(abilityName, resource, additionalProperties);
   },
 
-  cannot: function(abilityName, resource, aditionalProperties) {
-    return !this.can(abilityName, resource, aditionalProperties);
+  cannot: function(abilityName, resource, additionalProperties) {
+    return !this.can(abilityName, resource, additionalProperties);
   }
 });
