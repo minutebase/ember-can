5431c3d7da7bb046e91a2936b92287e9052c188c
kketch <wennerkevin@gmail.com>
2016-10-24 12:35:45 +0200

    Fix cannot recompute & destroy

--- addon/helpers/cannot.js
+++ addon/helpers/cannot.js
@@ -2,11 +2,18 @@ import Ember from 'ember';
 import getOwner from 'ember-getowner-polyfill';
 
 export default Ember.Helper.extend({
+  can: Ember.inject.service(),
+
   helper: Ember.computed(function() {
     return getOwner(this).lookup('helper:can');
   }),
 
   compute(params, hash) {
-    return !this.get('helper').compute(params, hash);
+    return !this.get('helper').compute.call(this, params, hash);
+  },
+
+  destroy() {
+    this.get('helper').destroy.call(this);
+    return this._super();
   }
 });
