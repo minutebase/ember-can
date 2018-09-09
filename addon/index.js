c0583cb4127a8b7245cfbdb5f3498d90be05d30c
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-09-09 16:25:39 -0700

    Remove deprecations

--- addon/index.js
+++ addon/index.js
@@ -1,37 +1,3 @@
 import Ability from 'ember-can/ability';
 
-// Deprecations
-
-// Deprecated usage of CanMixin
-import CanMixin from 'ember-can/mixins/can';
-
-// Deprecated import of 'computed' function
-import { ability } from 'ember-can/computed';
-import { deprecate } from '@ember/application/deprecations';
-
-function computed() {
-  deprecate('Importing "computed" from ember-can is deprecated and will be removed. Please migrate to `import { ability } from "ember-can/computed";`', false, {
-    id: 'ember-can.deprecate-computed-import',
-    until: '2.0.0',
-    url: 'https://github.com/minutebase/ember-can#looking-up-abilities'
-  });
-
-  return ability(...arguments);
-}
-
-// Deprecated import of 'CanService' service
-import Can from 'ember-can/services/can';
-
-let CanService = Can.extend({
-  init() {
-    this._super(...arguments);
-
-    deprecate('Importing "CanService" from ember-can is deprecated and will be removed. Please migrate to `import CanService from "ember-can/services/can";`', false, {
-      id: 'ember-can.deprecate-service-import',
-      until: '2.0.0',
-      url: 'https://github.com/minutebase/ember-can#looking-up-abilities'
-    });
-  }
-});
-
-export { Ability, CanMixin, CanService, computed };
+export { Ability };
--- addon/services/can.js
+++ addon/services/can.js
@@ -1,7 +1,6 @@
 import Service from '@ember/service';
 import { assert } from '@ember/debug';
 import { getOwner } from '@ember/application';
-import { deprecate } from '@ember/application/deprecations';
 import { assign } from '@ember/polyfills';
 
 import normalizeAbilityString from 'ember-can/utils/normalize';
@@ -11,12 +10,6 @@ export default Service.extend({
     return normalizeAbilityString(abilityString);
   },
 
-  build() {
-    deprecate('Usage of "build" is deprecated, use "abilityFor" instead.', false, {
-      id: 'ember-can.deprecate-service-build',
-      until: '2.0.0',
-      url: 'https://github.com/minutebase/ember-can#looking-up-abilities'
-    });
 
     return this.abilityFor(...arguments);
   },
