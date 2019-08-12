6dc50770b02a8ec49c29d089a2e614e8e51d06e4
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-08-12 17:47:54 +0200

    Fix tests

--- tests/integration/helpers/can-test.js
+++ tests/integration/helpers/can-test.js
@@ -6,6 +6,7 @@ import { Ability } from 'ember-can';
 import { computed } from '@ember/object';
 import Service from '@ember/service';
 import { inject as service } from '@ember/service';
+import { run } from '@ember/runloop';
 
 module('Integration | Helper | can', function(hooks) {
   setupRenderingTest(hooks);
@@ -141,144 +142,7 @@ module('Integration | Helper | can', function(hooks) {
       await render(hbs`{{if (can "write post") "true" "false"}}`);
       assert.dom(this.element).hasText('false');
 
-      this.owner.lookup('service:session').set('isLoggedIn', true);
-      assert.dom(this.element).hasText('true');
-    });
-  });
-
-  module('native class', function() {
-    test('it works with custom property parser', async function(assert) {
-      assert.expect(1);
-
-      this.owner.register('ability:post', class extends Ability {
-        parseProperty(propertyName) {
-          return propertyName; // without `can` prefix
-        }
-
-        worksWell = true
-      });
-
-      await render(hbs`{{if (can "works well post") "true" "false"}}`);
-      assert.dom(this.element).hasText('true');
-    });
-
-    test('it works without model', async function(assert) {
-      assert.expect(1);
-
-      this.owner.register('ability:post', class extends Ability {
-        canWrite = true
-      });
-
-      await render(hbs`{{if (can "write post") "true" "false"}}`);
-      assert.dom(this.element).hasText('true');
-    });
-
-    test('it can receives model', async function(assert) {
-      assert.expect(4);
-
-      this.owner.register('ability:post', class extends Ability {
-        @computed('model.write')
-        get canWrite() {
-          return this.get('model.write');
-        }
-      });
-
-      await render(hbs`{{if (can "write post" model) "true" "false"}}`);
-      assert.dom(this.element).hasText('false');
-
-      this.set('model', { write: false });
-      assert.dom(this.element).hasText('false');
-
-      this.set('model', { write: true });
-      assert.dom(this.element).hasText('true');
-
-      this.set('model', null);
-      assert.dom(this.element).hasText('false');
-    });
-
-    test('it works with default model', async function(assert) {
-      assert.expect(4);
-
-      this.owner.register('ability:post', class extends Ability {
-        model = { write: true }
-
-        @computed('model.write')
-        get canWrite() {
-          return this.get('model.write');
-        }
-      });
-
-      await render(hbs`{{if (can "write post" model) "true" "false"}}`);
-      assert.dom(this.element).hasText('true');
-
-      this.set('model', undefined);
-      assert.dom(this.element).hasText('true');
-
-      this.set('model', null);
-      assert.dom(this.element).hasText('false');
-
-      this.set('model', { write: false });
-      assert.dom(this.element).hasText('false');
-    });
-
-    test('it can receives properties', async function(assert) {
-      assert.expect(2);
-
-      this.owner.register('ability:post', class extends Ability {
-        @computed('write')
-        get canWrite() {
-          return this.get('write');
-        }
-      });
-
-      this.set('write', false);
-      await render(hbs`{{if (can "write post" write=write) "true" "false"}}`);
-      assert.dom(this.element).hasText('false');
-
-      this.set('write', true);
-      assert.dom(this.element).hasText('true');
-    });
-
-    test('it can receives model and properties', async function(assert) {
-      assert.expect(2);
-
-      this.owner.register('ability:post', class extends Ability {
-        @computed('model.write', 'write')
-        get canWrite() {
-          return this.get('model.write') && this.get('write');
-        }
-      });
-
-      this.set('write', false);
-      this.set('model', { write: false });
-      await render(hbs`{{if (can "write post" model write=write) "true" "false"}}`);
-      assert.dom(this.element).hasText('false');
-
-      this.set('write', true);
-      this.set('model', { write: true });
-      assert.dom(this.element).hasText('true');
-    });
-
-    test('it reacts on ability change', async function(assert) {
-      assert.expect(2);
-
-      this.owner.register('service:session', class extends Service {
-        isLoggedIn = false
-      });
-
-      this.owner.register('ability:post', class extends Ability {
-        @service session;
-
-        @computed('session.isLoggedIn')
-        get canWrite() {
-          return this.get('session.isLoggedIn');
-        }
-      });
-
-      await render(hbs`{{if (can "write post") "true" "false"}}`);
-      assert.dom(this.element).hasText('false');
-
-      this.owner.lookup('service:session').set('isLoggedIn', true);
+      run(() => this.owner.lookup('service:session').set('isLoggedIn', true));
       assert.dom(this.element).hasText('true');
     });
   });
