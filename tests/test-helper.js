db657091d450cc8ac617f8e1cfb4d506c9bd7f64
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-11-26 15:05:10 +0100

    Upgrade dependencies

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,9 +4,12 @@ import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import Sinon from 'sinon';
 
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
+
+QUnit.testDone(() => Sinon.restore());
 
 start();
