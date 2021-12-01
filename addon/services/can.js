5e8ba4f689fdbfd090715ecfdf8497ba5dc061d0
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-12-01 17:43:12 +0100

    Fix can service deprecation

--- addon/services/can.js
+++ addon/services/can.js
@@ -1,59 +1,88 @@
-import AbilitiesService from './abilities';
-import { deprecate } from '@ember/debug';
+import Service from '@ember/service';
+import Ability from 'ember-can/ability';
+import { assert } from '@ember/debug';
+import { getOwner } from '@ember/application';
 
-function deprecateMethod() {
-  deprecate(
-    'Using CanService has been deprecated in favor of AbilitiesService. You should replace all occurrences with new `abilities` service.',
-    false,
-    {
-      id: 'ember-can.can-service',
-      until: '5.0.0',
-      for: 'ember-can',
-      since: {
-        enabled: '4.1.0',
-      },
+import normalizeAbilityString from 'ember-can/utils/normalize';
+
+export default class CanService extends Service {
+  /**
+   * Parse abilityString into an object with extracted propertyName and abilityName
+   * eg. for 'create projects in account' -> `{ propertyName: 'createProjects', abilityName: 'account'}`
+   * @public
+   * @param  {String} string eg. 'create projects in account'
+   * @return {Object}        extracted propertyName and abilityName
+   */
+  parse(abilityString) {
+    return normalizeAbilityString(abilityString);
+  }
+
+  /**
+   * Create an instance of Ability
+   * @public
+   * @param  {String} abilityName     name of ability class
+   * @param  {*}      model
+   * @param  {Object} [properties={}] extra properties (to be set on the ability instance)
+   * @return {Ability}                Ability instance of requested ability
+   */
+  abilityFor(abilityName, model, properties = {}) {
+    let AbilityFactory = getOwner(this).factoryFor(`ability:${abilityName}`);
+
+    assert(`No ability type found for '${abilityName}'`, AbilityFactory);
+
+    if (typeof model !== 'undefined') {
+      properties = { model, ...properties };
     }
-  );
-}
 
-export default class CanService extends AbilitiesService {
-  /**
-   * @deprecated Use new AbilitiesService methods
-   */
-  parse() {
-    deprecateMethod();
-    return super.parse(...arguments);
+    let ability = AbilityFactory.create(properties);
+    assert(
+      `Ability '${abilityName}' has to inherit from ember-can Ability`,
+      ability instanceof Ability
+    );
+
+    return ability;
   }
 
   /**
-   * @deprecated Use new AbilitiesService methods
+   * Returns a value for requested ability in specified ability class
+   * @public
+   * @param  {String} propertyName name of ability, eg `createProjects`
+   * @param  {String} abilityName  name of ability class
+   * @param  {*}      model
+   * @param  {Object} properties   extra properties (to be set on the ability instance)
+   * @return {*}                   value of ability
    */
-  abilityFor() {
-    deprecateMethod();
-    return super.abilityFor(...arguments);
+  valueFor(propertyName, abilityName, model, properties) {
+    let ability = this.abilityFor(abilityName, model, properties);
+    let result = ability.getAbility(propertyName, model, properties);
+
+    ability.destroy();
+
+    return result;
   }
 
   /**
-   * @deprecated Use new AbilitiesService methods
+   * Returns `true` if ability is permitted
+   * @public
+   * @param  {[type]} abilityString eg. 'create projects in account'
+   * @param  {*}      model
+   * @param  {[type]} properties    extra properties (to be set on the ability instance)
+   * @return {Boolean}              value of ability converted to boolean
    */
-  valueFor() {
-    deprecateMethod();
-    return super.valueFor(...arguments);
+  can(abilityString, model, properties) {
+    let { propertyName, abilityName } = this.parse(abilityString);
+    return !!this.valueFor(propertyName, abilityName, model, properties);
   }
 
   /**
-   * @deprecated Use new AbilitiesService methods
+   * Returns `true` if ability is not permitted
+   * @public
+   * @param  {[type]} abilityString eg. 'create projects in account'
+   * @param  {*}      model
+   * @param  {[type]} properties    extra properties (to be set on the ability instance)
+   * @return {Boolean}              value of ability converted to boolean
    */
-  can() {
-    deprecateMethod();
-    return super.can(...arguments);
-  }
-
-  /**
-   * @deprecated Use new AbilitiesService methods
-   */
-  cannot() {
-    deprecateMethod();
-    return super.cannot(...arguments);
+  cannot(abilityString, model, properties) {
+    return !this.can(abilityString, model, properties);
   }
 }
