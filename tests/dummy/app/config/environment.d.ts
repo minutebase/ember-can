e4ead4dc260f305d4ca078e86e6089211cbd48b1
Charles Fries <charliefries@icloud.com>
2024-03-29 12:59:49 -0700

    Cleanup

--- tests/dummy/app/config/environment.d.ts
+++ tests/dummy/app/config/environment.d.ts
@@ -1,14 +1,14 @@
-export default config;
-
 /**
  * Type declarations for
- *    import config from 'my-app/config/environment'
+ *    import config from 'dummy/config/environment'
  */
 declare const config: {
   environment: string;
   modulePrefix: string;
   podModulePrefix: string;
-  locationType: string;
+  locationType: 'history' | 'hash' | 'none';
   rootURL: string;
   APP: Record<string, unknown>;
 };
+
+export default config;
