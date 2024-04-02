0e5d7f3c8d11c8c04a215861008d90e58bf994f7
Charles Fries <charliefries@icloud.com>
2024-04-02 12:33:02 -0700

    Revert

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,18 +7,18 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-4.8',
+        name: 'ember-lts-3.20',
         npm: {
           devDependencies: {
-            'ember-source': '~4.8.0',
+            'ember-source': '~3.20.5',
           },
         },
       },
       {
-        name: 'ember-lts-4.12',
+        name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            'ember-source': '~4.12.0',
+            'ember-source': '~3.24.3',
           },
         },
       },
@@ -43,6 +43,24 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
+          },
+        },
+      },
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false,
+          }),
+        },
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
+          ember: {
+            edition: 'classic',
           },
         },
       },
