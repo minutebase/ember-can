2efa77cb7c702e2a8ee594bc6e7df3a727e0a064
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-05-13 12:31:52 +0200

    Convert helpers to TS

---
+++ ember-can/src/helpers/cannot.ts
@@ -0,0 +1,22 @@
+import Helper from '@ember/component/helper';
+import { inject as service } from '@ember/service';
+import type Ability from '../services/abilities.ts';
+
+interface CannotSignature {
+  Args: {
+    Positional: never[];
+    Named: Record<string, unknown>;
+  };
+  Return: boolean;
+}
+
+export default class CannotHelper extends Helper<CannotSignature> {
+  @service declare abilities: Ability;
+
+  compute(
+    [abilityString, model]: CannotSignature['Args']['Positional'],
+    properties: CannotSignature['Args']['Named'] = {},
+  ): boolean {
+    return this.abilities.cannot(abilityString ?? '', model, properties);
+  }
+}
